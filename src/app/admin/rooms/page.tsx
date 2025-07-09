224003e2f4e05cb6cfa555feb6b13bd4f7a2f7d0
Jonathan Flores <jonathan.tobias.flores@gmail.com>
2025-07-09 23:06:28 +0200

    Fixed a layout bug on the admin rooms page where editing the last item in the list would cause the entire page to get a scrollbar.

--- src/app/admin/rooms/page.tsx
+++ src/app/admin/rooms/page.tsx
@@ -8,7 +8,7 @@ export default async function AdminRoomsPage() {
 
   return (
     <main className="flex justify-center bg-[#ECECEC]">
-      <div className="w-[393px] h-[852px] relative bg-[#ECECEC]">
+      <div className="w-[393px] h-[852px] relative bg-[#ECECEC] overflow-hidden">
         <div className="absolute w-[345px] top-[79px] left-6">
           <h1 className="w-full font-normal text-black text-[40px] tracking-[-1.20px] leading-10 font-['Roboto',_sans-serif]">
             Rumshantering
@@ -19,11 +19,11 @@ export default async function AdminRoomsPage() {
           <AddRoomForm />
         </div>
 
-        <div className="absolute w-[345px] top-[430px] left-6">
-          <h2 className="text-xl font-semibold mb-4 text-gray-700">
+        <div className="absolute w-[345px] top-[430px] left-6 right-6 bottom-0 flex flex-col">
+          <h2 className="text-xl font-semibold mb-4 text-gray-700 flex-shrink-0">
             Befintliga rum
           </h2>
-          <div className="space-y-4 h-[350px] overflow-y-auto hide-scrollbar">
+          <div className="flex-grow space-y-4 overflow-y-auto hide-scrollbar pr-2">
             {rooms.map((room) => (
               <RoomItem key={room.id} room={room} />
             ))}
